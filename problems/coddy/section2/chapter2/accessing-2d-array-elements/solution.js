4b7daf4cc16c165f256ce05bdfae76c34b763ff9
danimkim <danimkim.dev@gmail.com>
2026-04-30 22:12:08 +0100

    refactor the code by directly accessing the row by index

--- problems/coddy/section2/chapter2/accessing-2d-array-elements/solution.js
+++ problems/coddy/section2/chapter2/accessing-2d-array-elements/solution.js
@@ -1,6 +1,4 @@
 function getElementsInRow(grid, rowIndex) {
   if (rowIndex < 0 || rowIndex >= grid.length) return [];
-
-  return grid.filter((row, idx) => rowIndex === idx)[0];
+  return grid[rowIndex];
 }
-// Do not write anything outside the function
