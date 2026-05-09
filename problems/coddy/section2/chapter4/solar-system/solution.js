dfe0b0310b2efbab547a8c22edaf97fef36f1721
danimkim <danimkim.dev@gmail.com>
2026-05-10 00:58:20 +0100

    refactor the solution to improve code readability and update note

--- problems/coddy/section2/chapter4/solar-system/solution.js
+++ problems/coddy/section2/chapter4/solar-system/solution.js
@@ -2,48 +2,30 @@ function analyzeSolarSystem(solarSystemData) {
   // Write code here
   const BASE_YEAR = 2010;
   const parsedData = JSON.parse(solarSystemData);
-  const { planets } = parsedData;
 
-  const newPlanets = planets.map((planet) => {
+  parsedData.planets = parsedData.planets.map((planet) => {
     const { type, surfaceTemperature, numberOfMoons, discoveredIn, hasRings } =
       planet;
-    let newPlanet = { ...planet };
+    const newPlanet = { ...planet };
 
     if (type === 'rocky') {
-      newPlanet = {
-        ...newPlanet,
-        classification:
-          surfaceTemperature.max < 50
-            ? 'Potentially Habitable'
-            : 'Extreme Environment',
-      };
-    }
-    if (type === 'gas') {
-      newPlanet = {
-        ...newPlanet,
-        classification:
-          numberOfMoons > 10 ? 'Complex System' : 'Simple Gas Giant',
-      };
+      newPlanet.classification =
+        surfaceTemperature.max < 50
+          ? 'Potentially Habitable'
+          : 'Extreme Environment';
+    } else if (type === 'gas') {
+      newPlanet.classification =
+        numberOfMoons > 10 ? 'Complex System' : 'Simple Gas Giant';
     }
 
-    newPlanet = {
-      ...newPlanet,
-      ageOfDiscovery: BASE_YEAR - discoveredIn,
-    };
+    newPlanet.ageOfDiscovery = BASE_YEAR - discoveredIn;
 
     if (hasRings && numberOfMoons > 5) {
-      newPlanet = {
-        ...newPlanet,
-        specialFeature: 'Ring System with Rich Moon Population',
-      };
+      newPlanet.specialFeature = 'Ring System with Rich Moon Population';
     }
     return newPlanet;
   });
 
-  const updatedPlanets = {
-    planets: newPlanets,
-  };
-
-  return JSON.stringify(updatedPlanets);
+  return JSON.stringify(parsedData);
 }
 // Don't write anything outside the function
